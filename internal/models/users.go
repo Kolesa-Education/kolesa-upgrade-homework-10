581b6dca22c30d8ccc2c3a3fdf4aff86f3350b18
aabdrakhman <abdrahman.aygerim@gmail.com>
2022-11-07 00:04:35 +0600

    homework10

---
+++ internal/models/users.go
@@ -0,0 +1,43 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Name       string `json:"name"`
+	TelegramId int64  `json:"telegram_id"`
+	FirstName  string `json:"first_name"`
+	LastName   string `json:"last_name"`
+	ChatId     int64  `json:"chat_id"`
+	Tasks []Task  `gorm:"foreignKey:TelegramId;references:TelegramId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+}
+
+
+type UserModel struct {
+	Db *gorm.DB
+}
+
+func (m *UserModel) Create(user User) error {
+
+	result := m.Db.Create(&user)
+
+	return result.Error
+}
+
+func (m *UserModel) FindOne(telegramId int64) (*User, error) {
+	existUser := User{}
+  
+	result := m.Db.First(&existUser, User{TelegramId: telegramId})
+  
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	
+	return &existUser, nil
+ }
+
+ func (m *UserModel) GetAll() ([]User, error) {
+	var users []User
+	err := m.Db.Model(&User{}).Preload("Tasks").Find(&users).Error
+	return users, err
+}
