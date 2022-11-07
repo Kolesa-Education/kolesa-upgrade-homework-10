77e799bdc81cf863ee84be0d799fe09b5e83c816
Yelaman Fazyl <fazil.el2003@outlook.com>
2022-11-07 08:20:54 +0600

    выполняю задание 10

---
+++ internal/models/tasks.go
@@ -0,0 +1,45 @@
+package models
+
+import "gorm.io/gorm"
+
+type Task struct {
+	gorm.Model
+	Title		string	`json:"title"`
+	Description string	`json:"description"`
+	EndDate		string	`json:"enddate"`
+	TelegramID	int64	`json:"telegramID"`
+}
+
+type TaskModel struct {
+	Db *gorm.DB
+}
+
+func (m *TaskModel) Create(task Task) error {
+
+	result := m.Db.Create(&task)
+
+	return result.Error
+}
+
+func (m *TaskModel) Delete(id int) error {
+	task := Task{};
+
+	result := m.Db.Delete(&task, id)
+
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+func (m *TaskModel) GetAll(telegramId int64) ([]Task, error) {
+	tasks := []Task{};
+	result := m.Db.Where("telegram_id = ?", telegramId).Find(&tasks)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return tasks, nil
+}
