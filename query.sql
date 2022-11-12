05bedb3a3c387075f8b0d0c89a3f0c5495315010
Evgeniy Smolnikov <evg.v.sml@gmail.com>
2022-11-12 13:55:09 +0600

    Выполняю задание 10

---
+++ query.sql
@@ -0,0 +1,21 @@
+CREATE TABLE
+    users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name varchar(255),
+        telegram_id INT,
+        first_name varchar(255),
+        last_name varchar(255),
+        chat_id INT
+    );
+
+
+CREATE TABLE
+    tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title varchar(255),
+        description varchar(255),
+        end_date date,
+        user_id INTEGER NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+    );
+    
